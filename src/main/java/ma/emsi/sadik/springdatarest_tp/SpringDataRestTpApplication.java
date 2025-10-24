78f22d3a49ea98163bd77287338bb6e671a69814
SADIK Oussama <Oussama.sadik@emsi-edu.ma>
2025-10-24 12:35:59 +0100

    tp commit

---
+++ src/main/java/ma/emsi/sadik/springdatarest_tp/SpringDataRestTpApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.sadik.springdatarest_tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestTpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestTpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/emsi/sadik/springdatarest_tp/SpringDataRestTpApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.sadik.springdatarest_tp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestTpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
